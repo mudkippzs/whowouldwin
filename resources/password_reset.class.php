b198836fd011e3df6012e69db37953bc1fcd2f8b
Alex <mudkippzs@gmail.com>
2016-07-10 21:51:51 +0100

    another damn test of the forgot password mail

--- resources/password_reset.class.php
+++ resources/password_reset.class.php
@@ -16,7 +16,7 @@ class pass_reset extends userService{
 	
 	public function make_new_pass(){		
 		$this->newHash = $this->random_pass($this->generate_user_id(),$this->user_id);		
-		$hash = $this->newHash['m'];		
+		$hash = $this->result['m'];		
 		$user = $this->post_card($this->user_id);
 		$env = array($user['email'],'noreply@whowouldwin.io','Who Would Win','Password has been reset!',$hash,"Hi " . ucfirst($user['username']) . " <p>Your new password is: {{MESSAGE_BODY}} </p><p>Please update it soon to something you wont forget!</p><p>Thanks,</br><br>Alex<br><strong>Admin</strong></p>");
 		$mail = new mailTemplate();
