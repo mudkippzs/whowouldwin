b85b6b69982e60eb08509c92d844abecd6894165
mud <mud@vps297664.ovh.net>
2016-07-10 17:19:24 +0000

    added mail.class.php

---
+++ resources/mail.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class mailTemplate{
+	
+	protected $to;
+	protected $from;
+	protected $fromName;
+	protected $messageContent;
+	protected $template;
+
+	public function __construct($to = array(),$from = null,$fromName = null,$messageContent = 'Empty',$template = 'base.tpl'){
+	
+	}
+
+
+	
+
+
+
+
+}
+
+
+?>
