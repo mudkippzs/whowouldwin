99c2c0115fcd4a2308a75b54cd4058df7694b392
Alex <mudkippzs@gmail.com>
2016-07-11 23:08:51 +0100

    stupid navigation label generation is being a c*nt

--- resources/functions.php
+++ resources/functions.php
@@ -58,6 +58,7 @@ function print_nav($currentPage){
 		echo "<ul>";
 		foreach ($stubs as $s) {
 			if(is_null($s['stub'])){
+				echo $s['stub'];
 				$s['stub'] = 'HOME';
 			}
 			print_r ("<li><a href='/index.php?page=" . $s['stub'] . "'>" . strtoupper($s['page_title']) . "</a></li>");
